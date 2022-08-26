b747ab700abf51c0a339af657a800a15381fab96
shubham-rawal <shubhamrawal2001@gmail.com>
2022-08-26 16:19:04 +0530

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Shubham Rawal on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
