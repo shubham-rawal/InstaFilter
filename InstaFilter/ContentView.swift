5d1441ddfb9f8565cc616d89e4774eabdfae2a8b
shubham-rawal <shubhamrawal2001@gmail.com>
2022-08-26 17:10:03 +0530

    Demonstration of CoreImage filters

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,12 +5,56 @@
 //  Created by Shubham Rawal on 26/08/22.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image : Image?
+    @State private var amount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            
+            Slider(value: $amount, in: 0...10)
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "SWAR") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        currentFilter.inputImage = beginImage
+
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        //changing the filter parameters dynamically
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 100, forKey: kCIInputRadiusKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImg = UIImage(cgImage: cgimg)
+            
+            image = Image(uiImage: uiImg)
+        }
     }
 }
 
